788f6193af023137d32bc31f2c5999c0ac9d5cdc
sebastian <angelserial11@gmail.com>
2022-12-29 20:20:59 -0600

    Correcion de base de datos (db.php).

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "sebgameYT1012", "e_solar")or die
+$conexion = mysqli_connect("localhost", "root", "sebgameYT1012", "p_herbalife")or die
 ("Problemas con la conexion.");
 
-mysqli_query($conexion, "INSERT into usuarios(Nombre, Ap, Am, Email, Clave, R_Clave)values
-('$_REQUEST[Nombre]', '$_REQUEST[Ap]', '$_REQUEST[Am]', '$_REQUEST[Email]', '$_REQUEST[Clave]', '$_REQUEST[R_Clave]')")or die
+mysqli_query($conexion, "INSERT into usuarios(nombre, apellidos, n_cel, email, mensaje)values
+('$_REQUEST[nombre]', '$_REQUEST[apellidos]', '$_REQUEST[n_cel]', '$_REQUEST[email]', '$_REQUEST[mensaje]')")or die
 ("Problemas en el select".mysqli_error($conexion));
 
 mysqli_close($conexion);
 
-echo("Te has registrado correctamente.");
+echo("Tus datos y mensaje se han enviado correctamente, puedes cerrar esta pestaña.");
 ?>
